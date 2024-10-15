1033d40c7d2a9294d61fef08796b3fbc3451f11f
Геннадий <gemiyur@live.ru>
2024-10-15 11:03:55 +0300

    Добавьте файлы проекта.

---
+++ IconEditor/Comparers/CompareResult.cs
@@ -0,0 +1,23 @@
+﻿namespace IconEditor.Comparers
+{
+    /// <summary>
+    /// Статический класс методов для результата сравнения.
+    /// </summary>
+    public static class CompareResult
+    {
+        /// <summary>
+        /// Возвращает инвертированный результат сравнения.
+        /// </summary>
+        /// <param name="compareResult">Результат сравнения.</param>
+        /// <returns>Инвертированный результат сравнения.</returns>
+        public static int Invert(int compareResult)
+        {
+            if (compareResult < 0)
+                return 1;
+            if (compareResult > 0)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}

b70e90fa03d68b756fe77db53d361ad59c14dd17
Геннадий <gemiyur@live.ru>
2024-11-08 22:34:42 +0300

    Создал проект IconsApp для подбора значков.

---
+++ IconEditor/IconsApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IconsApp
+{
+    /// <summary>
+    /// Класс приложения.
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
